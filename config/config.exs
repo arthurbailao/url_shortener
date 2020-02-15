a7a7bfcd9a6ca1fd36bf8301b935d89b591e59aa
Arthur Bailão <arthurbailao@gmail.com>
2020-02-15 11:37:51 -0300

    configurations

--- config/config.exs
+++ config/config.exs
@@ -15,7 +15,52 @@ config :url_shortener, UrlShortenerWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "YAPEZlapOFoOyZlvEsrbZ7E571OLFzr0MI0N3HyMUJJehXe1Ap2zz/zV1Koj6wS3",
   render_errors: [view: UrlShortenerWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: UrlShortener.PubSub, adapter: Phoenix.PubSub.PG2]
+  instrumenters: [UrlShortenerWeb.Instrumenter.Phoenix]
+
+config :prometheus, UrlShortenerWeb.Instrumenter.Phoenix,
+  controller_call_labels: [:controller, :action],
+  duration_buckets: [
+    10,
+    25,
+    50,
+    100,
+    250,
+    500,
+    1000,
+    2500,
+    5000,
+    10_000,
+    25_000,
+    50_000,
+    100_000,
+    250_000,
+    500_000,
+    1_000_000,
+    2_500_000,
+    5_000_000,
+    10_000_000
+  ],
+  registry: :default,
+  duration_unit: :microseconds
+
+config :prometheus, UrlShortenerWeb.Instrumenter.Pipeline,
+  labels: [:status_class, :method, :host, :scheme, :request_path],
+  duration_buckets: [
+    10,
+    100,
+    1_000,
+    10_000,
+    100_000,
+    300_000,
+    500_000,
+    750_000,
+    1_000_000,
+    1_500_000,
+    2_000_000,
+    3_000_000
+  ],
+  registry: :default,
+  duration_unit: :microseconds
 
 # Configures Elixir's Logger
 config :logger, :console,
